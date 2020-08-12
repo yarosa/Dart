5bc383cf40936bdbc68b9e21ec37ef0da0741974
yarosa <yarosa@gmail.com>
2020-08-13 00:11:39 +0300

    files added

---
+++ helloworldfansy.dart
@@ -0,0 +1,13 @@
+//helloworldfansy.dart
+import 'dart:html';
+void main() {
+  querySelector("#button").onClick.listen(seyHello);  
+}
+void seyHello(MouseEvent event) {
+querySelector("#name").text = (querySelector("#name_box") as InputElement) value;
+querySelector("#name_box") as InputElement) value = "";  
+}
+
+
+
+
